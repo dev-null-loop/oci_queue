f77b2ae4cfed2976285de81f6c78c44eab6f5716
dev-null-loop <dev.null.loop@gmail.com>
2026-05-18 15:47:08 +0300

    Fix QUEUE variable descriptions

--- consumer_group/variables.tf
+++ consumer_group/variables.tf
@@ -1,39 +1,39 @@
 variable "consumer_group_filter" {
-  description = "The filter used by the consumer group. Only messages matching the filter will be available by consumers of the group. The primary consumer group cannot have any filter."
+  description = "(Optional) (Updatable) The filter used by the consumer group. Only messages matching the filter will be available by consumers of the group. The primary consumer group cannot have any filter."
   type        = string
   default     = null
 }
 
 variable "dead_letter_queue_delivery_count" {
-  description = "The number of times a message can be delivered to a consumer before being moved to the dead letter queue.  A value of 0 indicates that the DLQ is not used. If the value isn't specified, it will be using the value defined at the queue level."
+  description = "(Optional) (Updatable) The number of times a message can be delivered to a consumer before being moved to the dead letter queue.  A value of 0 indicates that the DLQ is not used. If the value isn't specified, it will be using the value defined at the queue level."
   type        = number
   default     = null
 }
 
 variable "defined_tags" {
-  description = "Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`"
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`"
   type        = map(string)
   default     = null
 }
 
 variable "display_name" {
-  description = "The user-friendly name of the consumer group."
+  description = "(Required) (Updatable) The user-friendly name of the consumer group."
   type        = string
 }
 
 variable "freeform_tags" {
-  description = "Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
+  description = "(Optional) (Updatable) Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
   type        = map(string)
   default     = {}
 }
 
 variable "is_enabled" {
-  description = "Used to enable or disable the consumer group.  An enabled consumer group will have a lifecycle state of ACTIVE, while a disabled will have its state as INACTIVE."
+  description = "(Optional) (Updatable) Used to enable or disable the consumer group.  An enabled consumer group will have a lifecycle state of ACTIVE, while a disabled will have its state as INACTIVE."
   type        = bool
   default     = null
 }
 
 variable "queue_id" {
-  description = "The OCID of the associated queue."
+  description = "(Required) The OCID of the associated queue."
   type        = string
 }
